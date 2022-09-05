bdc18c2b23ecbd3ed5c62fb87236d4229dea6f35
Cevahir Sena Esenboğa <84200306+csenaesenboga@users.noreply.github.com>
2022-09-05 21:58:49 +0300

    change Math.pow() method

--- loops/LoopThree.java
+++ loops/LoopThree.java
@@ -1,6 +1,6 @@
 //Takes a number, prints the powers of 4 and 5 until the input number
 import java.util.Scanner;
-import java.lang.Math;
+
 
 public class LoopThree {
     public static void main(String[] args) {
@@ -10,10 +10,10 @@ public class LoopThree {
         System.out.print("Please enter a number: ");
         k = s.nextInt();
         System.out.println("Powers of 4");
-        for(int i = 0; i<=k;i++) System.out.print(Math.pow(4,i) + " ");
+        for(int i = 1; i<=k;i*=4) System.out.print(i + " ");
 
         System.out.println("");
         System.out.println("Powers of 5");
-        for(int i = 0; i<=k;i++) System.out.print(Math.pow(5,i) + " ");
+        for(int i = 1; i<=k;i*=5) System.out.print(i + " ");
     }
 }
